7920ce24fbb018980b2288e21b06a797826582fc
BrandonMFong <fong.m.brandon97@gmail.com>
2021-02-20 17:40:33 -0800

    Adding sbox

--- main.go
+++ main.go
@@ -2,14 +2,47 @@
 // Without consulting any of the numerous public-domain implementations available,
 // implement AES, on your own, from the spec or from the description provided by this chapter. Then
 // test your implementation according to the test vectors provided in the AES documentation.
+// https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/aes/AESAVS.pdf
 
 package main
 
-import "fmt"
+var rounds uint = 10
 
 func main() {
-	message := "Hello"
-	byteMessage := []byte(message)
+	var message byte
 
-	fmt.Println(byteMessage)
+	message = 0x42
+
+	message = AES(message)
+}
+
+// AES is a function
+func AES(message byte) byte {
+	var result byte // the 's'
+	var originalKey byte
+	var keys []byte
+	var index uint
+
+	// Expand
+	keys = expand(originalKey)
+
+	// S
+	result = message
+
+	index = 0
+	for index < rounds {
+		// Shift rows
+
+		// mix columns
+
+		index++
+	}
+
+	return result
+}
+
+func expand(inputString byte) []byte {
+	var result []byte
+
+	return result
 }
